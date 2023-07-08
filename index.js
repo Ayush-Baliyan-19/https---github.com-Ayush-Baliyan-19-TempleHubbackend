775ae6398a38ef7158358aa1656a67e1cbb3cc40
Ayush-Baliyan-19 <ayush.3316403221@ipu.ac.in>
2023-07-08 08:22:53 +0530

    postal api updated

--- index.js
+++ index.js
@@ -14,17 +14,20 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/address/cityforPincode", async (req,res)=>{
-    const pincode = req.query.pincode;
-    const response= await fetch(`https://www.postpincode.in/api/getCityName.php?pincode=${pincode}`,{
-        method:"GET",
-        headers:{
-            "Content-Type":"application/json"
-        }
-    })
-    const data= await response.json();
-    if(!data)
+    try {
+        const pincode = req.query.pincode;
+        const response= await fetch(`https://www.postpincode.in/api/getCityName.php?pincode=${pincode}`,{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        const data = await response.json();
+        if(!data)
         res.status(400).send("No data found")
-    res.status(200).send(data)
+    } catch (error) {   
+        res.status(200).send(error)
+    }
 })
 
 app.use("/api/auth",require("./routes/auth"))
