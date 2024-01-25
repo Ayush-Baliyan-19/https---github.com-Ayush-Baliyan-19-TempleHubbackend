85c4df4fa6694356fef2fd805973025a59666b69
ayush-baliyan-19 <ayushbaliyan05@gmail.com>
2024-01-25 17:59:03 +0530

    Server Updated

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app= express();
 const dotenv=require("dotenv")
 const cors= require("cors")
 const bodyParser= require("body-parser")
-
-const ConnectToDB= require("./DB")
+const axios = require('axios');
+const ConnectToDB= require("./DB");
 
 dotenv.config();
 
@@ -19,6 +19,22 @@ app.use(bodyParser.json({limit: "100mb"}));
 app.use(bodyParser.urlencoded({limit: "100mb",extended:true}));
 
 app.use(express.urlencoded({limit: "10mb",extended:true}));
+
+
+app.get("/api/test",(req,res)=>{
+    return res.status(200).json({message:"Server is up and running"})
+})
+
+const sendPingRequest = async () => {
+    try {
+        const response = await axios.get("http://localhost:8080/api/test"); // Replace with your server's URL
+        console.log("Ping successful:", response.data);
+    } catch (error) {
+        console.error("Error pinging the server:", error.message);
+    }
+};
+
+setInterval(sendPingRequest, 15 * 60 * 1000);
 
 app.use("/api/auth",require("./routes/auth"))
 app.use("/api/user",require("./routes/user"))
